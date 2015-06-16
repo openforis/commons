34eec61da97e14d518bbac0fe2533367793372ee
Stefano Ricci <stefano.ricci@fao.org>
2015-06-16 18:32:28 +0200

    Store reference to jobs in job manager

--- commons-concurrency/src/main/java/org/openforis/concurrency/SimpleJobManager.java
+++ commons-concurrency/src/main/java/org/openforis/concurrency/SimpleJobManager.java
@@ -1,9 +1,12 @@
 package org.openforis.concurrency;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -14,15 +17,35 @@ import java.util.concurrent.Executors;
  */
 public class SimpleJobManager implements JobManager {
 	
-	private Collection<String> locks;
+	protected static final long MAX_JOB_IDLE_MILLIS = 30 * 60 * 1000; //30 minutes
+	private static final long JOB_INFO_UPDATE_PERIOD_MILLIS = 60 * 1000; //1 minute
+	
 	private Map<String, Job> jobByLockId;
+	private Map<String, JobInfo> jobInfoById;
 	
 	private Executor jobExecutor;
 	
+	private Timer jobInfoUpdateTimer;
+	
 	public SimpleJobManager() {
 		jobByLockId = new HashMap<String, Job>();
-		locks = new HashSet<String>();
+		jobInfoById = new HashMap<String, JobInfo>();
 		jobExecutor = Executors.newCachedThreadPool();
+		initJobInfoUpdateTimer();
+	}
+
+	public void destroy() {
+		jobInfoUpdateTimer.cancel();
+	}
+	
+	private void initJobInfoUpdateTimer() {
+		jobInfoUpdateTimer = new Timer();
+		jobInfoUpdateTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				pruneIdleJobs();
+			}
+		}, JOB_INFO_UPDATE_PERIOD_MILLIS, JOB_INFO_UPDATE_PERIOD_MILLIS);
 	}
 	
 	@Override
@@ -64,6 +87,8 @@ public class SimpleJobManager implements JobManager {
 	}
 	
 	synchronized public <J extends Job> void start(final J job, final String lockId, boolean async) {
+		jobInfoById.put(job.getId().toString(), new JobInfo(job));
+		
 		job.init();
 		
 		if ( job.isPending() ) {
@@ -92,20 +117,24 @@ public class SimpleJobManager implements JobManager {
 	}
 
 	protected <J extends Job> void lock(final J job, final String lockId) {
-		if ( locks.contains(lockId) && jobByLockId.containsKey(lockId) && jobByLockId.get(lockId).isRunning() ) {
+		if ( jobByLockId.containsKey(lockId) && jobByLockId.get(lockId).isRunning() ) {
 			throw new RuntimeException("Another job is runnign for the same locking group: " + lockId);
 		} else {
-			locks.add(lockId);
 			jobByLockId.put(lockId, job);
 		}
 	}
 
-	public Job getJob(String lockId) {
+	public Job getJob(String jobId) {
+		JobInfo jobInfo = jobInfoById.get(jobId);
+		return jobInfo == null ? null : jobInfo.getJob();
+	}
+
+	public Job getLockingJob(String lockId) {
 		return jobByLockId.get(lockId);
 	}
 	
 	protected synchronized void release(String lockId) {
-		locks.remove(lockId);
+		jobByLockId.remove(lockId);
 	}
 	
 	protected <J extends Job> void runJob(final J job, final String lockId) {
@@ -113,7 +142,7 @@ public class SimpleJobManager implements JobManager {
 			job.run();
 		} finally {
 			if ( lockId != null ) {
-				locks.remove(lockId);
+				release(lockId);
 			}
 		}
 	}
@@ -125,4 +154,46 @@ public class SimpleJobManager implements JobManager {
 	protected void setJobExecutor(Executor jobExecutor) {
 		this.jobExecutor = jobExecutor;
 	}
+	
+	private synchronized void pruneIdleJobs() {
+		Set<Entry<String,JobInfo>> entrySet = jobInfoById.entrySet();
+		Iterator<Entry<String, JobInfo>> iterator = entrySet.iterator();
+		while (iterator.hasNext()) {
+			Entry<String, JobInfo> entry = (Entry<String, JobInfo>) iterator.next();
+			JobInfo jobInfo = entry.getValue();
+			Job job = jobInfo.getJob();
+			if (! job.isRunning() && ! job.isPending()) {
+				jobInfo.incrementIdleMillis(JOB_INFO_UPDATE_PERIOD_MILLIS);
+				if (jobInfo.getIdleMillis() > MAX_JOB_IDLE_MILLIS) {
+					iterator.remove();
+				}
+			}
+		}
+	}
+	
+	private class JobInfo {
+		
+		private Job job;
+		private long idleMillis;
+		
+		public JobInfo(Job job) {
+			super();
+			this.job = job;
+			this.idleMillis = 0;
+		}
+
+		public Job getJob() {
+			return job;
+		}
+		
+		public long getIdleMillis() {
+			return idleMillis;
+		}
+		
+		public void incrementIdleMillis(long amount) {
+			idleMillis += amount;
+		}
+		
+	}
+	
 }
