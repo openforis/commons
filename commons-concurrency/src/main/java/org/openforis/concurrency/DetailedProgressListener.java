af1bbd421c32a0b10a62776f78e67505a908b8c8
fefrain <fefrain@users.noreply.github.com>
2015-12-15 10:06:58 +0100

    Improve progress listener

--- commons-concurrency/src/main/java/org/openforis/concurrency/DetailedProgressListener.java
+++ commons-concurrency/src/main/java/org/openforis/concurrency/DetailedProgressListener.java
@@ -11,23 +11,41 @@ public interface DetailedProgressListener extends ProgressListener {
 	
 	public class Progress {
 		
-		private int completionPercent;
+		private long processedItems;
+		private long totalItems;
 		private Long elapsedTime;
 		private Long remainingTime;
 		
-		public Progress(int completionPercent) {
-			this(completionPercent, null, null);
+		public Progress(long processedItems, long totalItems) {
+			this(processedItems, totalItems, null, null);
 		}
 
-		public Progress(int completionPercent, Long elapsedTime, Long remainingTime) {
+		public Progress(long processedItems, long totalItems, Long elapsedTime, Long remainingTime) {
 			super();
-			this.completionPercent = completionPercent;
+			this.processedItems = processedItems;
+			this.totalItems = totalItems;
 			this.elapsedTime = elapsedTime;
 			this.remainingTime = remainingTime;
 		}
 
 		public int getCompletionPercent() {
-			return completionPercent;
+			return totalItems <= 0 ? -1 : Double.valueOf(Math.floor((double) (processedItems / totalItems) * 100)).intValue();
+		}
+		
+		public long getProcessedItems() {
+			return processedItems;
+		}
+		
+		public void setProcessedItems(long processedItems) {
+			this.processedItems = processedItems;
+		}
+		
+		public long getTotalItems() {
+			return totalItems;
+		}
+		
+		public void setTotalItems(long totalItems) {
+			this.totalItems = totalItems;
 		}
 		
 		public Long getElapsedTime() {
