a156b8a9fe78bac78d837839372aa6252af4cab3
fefrain <fefrain@users.noreply.github.com>
2015-12-15 09:31:41 +0100

    Introduce detailed progress listener

--- commons-concurrency/src/main/java/org/openforis/concurrency/ProgressListener.java
+++ commons-concurrency/src/main/java/org/openforis/concurrency/ProgressListener.java
@@ -1,12 +1,20 @@
 package org.openforis.concurrency;
 
+/**
+ * 
+ * @author S. Ricci
+ * @author D. Wiell
+ *
+ */
 public interface ProgressListener {
 
 	void progressMade();
 	
 	static ProgressListener NULL_PROGRESS_LISTENER = new ProgressListener() {
 		@Override
-		public void progressMade() {
-		}
+		public void progressMade() {}
 	};
+	
 }
+
+
