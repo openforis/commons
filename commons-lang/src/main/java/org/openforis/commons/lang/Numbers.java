74c08fffaed9aa3f65b1791c234d372058996785
fefrain <fefrain@users.noreply.github.com>
2016-03-10 16:29:01 +0100

    Add numbers helper class

---
+++ commons-lang/src/main/java/org/openforis/commons/lang/Numbers.java
@@ -0,0 +1,71 @@
+package org.openforis.commons.lang;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 
+ * @author S. Ricci
+ *
+ */
+public class Numbers {
+
+	public static int toInt(String value) {
+		return toInt(value, 0);
+	}
+	
+	public static int toInt(String value, int defaultValue) {
+		Integer result = toIntegerObject(value);
+		return result == null ? defaultValue : result;
+	}
+	
+	public static Integer toIntegerObject(String value) {
+		if (StringUtils.isBlank(value)) {
+			return null;
+		}
+		try {
+			return Integer.parseInt(value);
+		} catch(NumberFormatException e) {
+			return null;
+		}
+	}
+
+	public static double toDouble(String value) {
+		return toDouble(value, 0);
+	}
+	
+	public static double toDouble(String value, double defaultValue) {
+		Double result = toDoubleObject(value);
+		return result == null ? defaultValue : result;
+	}
+	
+	public static Double toDoubleObject(String value) {
+		if (StringUtils.isBlank(value)) {
+			return null;
+		}
+		try {
+			return Double.parseDouble(value);
+		} catch(NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	public static long toLong(String value) {
+		return toInt(value, 0);
+	}
+	
+	public static long toLong(String value, long defaultValue) {
+		Long result = toLongObject(value);
+		return result == null ? defaultValue : result;
+	}
+	
+	public static Long toLongObject(String value) {
+		if (StringUtils.isBlank(value)) {
+			return null;
+		}
+		try {
+			return Long.parseLong(value);
+		} catch(NumberFormatException e) {
+			return null;
+		}
+	}
+}
